6f0d84fb7dd14a23237c55d6a332167d0267e2a4
Jamie Ly <jamie.ly@gmail.com>
2012-06-04 18:42:40 -0400

    Added method defns to make specs passable.

--- src/connect_four/Game.scala
+++ src/connect_four/Game.scala
@@ -4,6 +4,7 @@ import connect_four._
 
 class Game {
 	val board = new Board
+	var currentMarker = Markers.A
 	// directions are deltas used to check board locations in the cardinal directions
 	val directions = for {
 	  i <- List.range(-1, 2); 
@@ -49,8 +50,47 @@ class Game {
 	  }
 	}
 	
+	def getFirstEmptyRowInColumn(c: Int): Option[Int] = {
+	  if(!board.isInBounds(new Index(0, c))) {
+	    None
+	  }
+	  else {
+	    def helper(row: Int): Int = {
+	      if(row < 0) {
+	        row
+	      }
+	      else {
+	        val marker = markerAt(new Index(row, c))
+	        if(marker == Some(Markers.Empty)) row else helper(row - 1)
+	      }
+	    }
+	    for {
+	      r <- Some(helper(board.size.height-1))
+	      result <- if(r < 0) None else Some(r)
+	    } yield result
+	  }
+	} 
+	
+	def toggleMarker(): Int = {
+	  currentMarker = if(currentMarker == Markers.A) Markers.B else Markers.A
+	  currentMarker
+	}
+	
+	def updateBoard(index: Index): Int = {
+	  board.move(currentMarker, index)
+	  toggleMarker()
+	}
+	
 	def move(mv: Move): Option[Int] = {
-	  None
+	  if(isWin()) {
+	    None
+	  }
+	  else {
+	    for {
+	      row <- getFirstEmptyRowInColumn(mv.col)
+	      result <- if(row >= 0) Some(updateBoard(new Index(row, mv.col))) else None
+	    } yield result
+	  }
 	}
 	
 	def markerAt(index:Index): Option[Int] = {
@@ -58,6 +98,6 @@ class Game {
 	}
 	
 	def getCurrentMarker(): Int = {
-	  Markers.Empty
+	  currentMarker
 	}
 }
